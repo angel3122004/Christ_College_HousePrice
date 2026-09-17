9dde88a2b73c9f036ef405adb4d887c740a7ed2b
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-17 13:59:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 
 model=joblib.load("MultiLinear_Regression_HousePrice_Model.pkl")
 st.title("House Price Prediction")
-area=st.number_input("Enter Area:", min_value=100.0 ,max_value=10000.0, value=600.0)
+area=st.number_input("Enter Area:", min_value=300.0 ,max_value=10000.0, value=600.0)
 bedrooms=st.number_input("Enter no of bedrooms:", min_value=1 ,max_value=10, value=1)
 floors=st.number_input("Enter no of floors:", min_value=0 ,max_value=10, value=0)
 
