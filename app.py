25bb26e469cc7db18aa047cb8fc6c0b819e050a6
angel3122004 <angelmary23cs@cce.edu.in>
2026-09-17 14:19:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import joblib
-
-model=joblib.load("MultiLinear_Regression_HousePrice_Model.pkl")
+model = joblib.load("MultiLinear_Regression_HousePrice_Model.pkl")
 st.title("House Price Prediction")
-area=st.number_input("Enter Area:", min_value=600.0 ,max_value=5000.0, value=600.0)
-bedrooms=st.number_input("Enter no of bedrooms:", min_value=1 ,max_value=10, value=1)
-floors=st.number_input("Enter no of floors:", min_value=0 ,max_value=10, value=0)
+
+area = st.number_input("Enter Area:", value=600.0)
+bedrooms = st.number_input("Enter no of bedrooms:", value=1)
+floors = st.number_input("Enter no of floors:", value=0)
 
 if st.button("Predict"):
-  if area<600.0 or area>3000.0:
-    st. error("area must be within 600-5000") 
-  elif bedrooms < 1 or bedrooms > 10:
-        st.error("Number of bedrooms must be between 1 and 10.")
-  elif floors < 0 or floors > 10:
-        st.error("Number of floors must be less than 10.")
-  prediction=model.predict([[area,bedrooms,floors]])
-  st.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
+    if area < 600 or area > 5000:
+        st.error("Area must be within 600-5000") 
+        st.stop() 
+    elif bedrooms <= 0:
+        st.error("Bedrooms cannot be 0")
+        st.stop()
+    elif bedrooms > 10:
+        st.error("Number of Bedrooms must be less than or equal to 10")
+        st.stop()
+    elif floors < 0 or floors > 10:
+        st.error("Number of floors must be between 0 and 10")
+        st.stop()
+    prediction = model.predict([[area, bedrooms, floors]])
+    st.success(f"Predicted price: {prediction[0]:.2f} Lakhs")
